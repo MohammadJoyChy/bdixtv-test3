8c2c3f4eef1e97ebb87609e2e91476bbdeddbc4d
BDIXTV <159545358+MohammadJoyChy@users.noreply.github.com>
2025-04-18 18:04:31 +0600

    Update test.php

--- api/test.php
+++ api/test.php
@@ -1,10 +1,6 @@
 <?php
 
 $id = @$_GET['id'];
-if (!$id) {
-    die("No ID provided.");
-}
-
 $user_ip = $_SERVER['REMOTE_ADDR'];
 $currentTimestamp = time();
 $portal = "tatatv.cc/";
@@ -14,8 +10,8 @@ $deviceid2 = "7340326103379BD8FEA27B1C14A38307DA3D9B09104D3A673AE8185AB5A68728";
 $serial = "5F00E464D4159";
 $sig = "";
 
-// Step 1: Handshake
 $n1 = "http://$portal/stalker_portal/server/load.php?type=stb&action=handshake&prehash=false&JsHttpRequest=1-xml";
+
 $h1 = [
     "Cookie: mac=$mac; stb_lang=en; timezone=GMT",
     "X-Forwarded-For: $user_ip",
@@ -30,7 +26,7 @@ curl_setopt($c1_curl, CURLOPT_SSL_VERIFYHOST, false);
 curl_setopt($c1_curl, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($c1_curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($c1_curl, CURLOPT_HTTPHEADER, $h1);
-curl_setopt($c1_curl, CURLOPT_USERAGENT, $h1[3]);
+curl_setopt($c1_curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3');
 $res1 = curl_exec($c1_curl);
 curl_close($c1_curl);
 
@@ -38,7 +34,6 @@ $response = json_decode($res1, true);
 $token = $response['js']['random'];
 $real = $response['js']['token'];
 
-// Step 2: Get Profile
 $h2 = [
     "Cookie: mac=$mac; stb_lang=en; timezone=GMT",
     "X-Forwarded-For: $user_ip",
@@ -56,12 +51,11 @@ curl_setopt($c2_curl, CURLOPT_SSL_VERIFYHOST, false);
 curl_setopt($c2_curl, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($c2_curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($c2_curl, CURLOPT_HTTPHEADER, $h2);
-curl_setopt($c2_curl, CURLOPT_USERAGENT, $h2[3]);
+curl_setopt($c2_curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3');
 $res2 = curl_exec($c2_curl);
 curl_close($c2_curl);
 
-// Step 3: Create Link with ID at the start
-$n3 = "http://$portal/stalker_portal/server/load.php?type=itv&action=create_link&cmd=ffrt%http://localhost/$id/ch/&JsHttpRequest=1-xml";
+$n3 = "http://$portal/stalker_portal/server/load.php?type=itv&action=create_link&cmd=ffrt%http://localhost/ch/$id&JsHttpRequest=1-xml";
 
 $c3_curl = curl_init();
 curl_setopt($c3_curl, CURLOPT_URL, $n3);
@@ -69,13 +63,14 @@ curl_setopt($c3_curl, CURLOPT_SSL_VERIFYHOST, false);
 curl_setopt($c3_curl, CURLOPT_SSL_VERIFYPEER, false);
 curl_setopt($c3_curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($c3_curl, CURLOPT_HTTPHEADER, $h2);
-curl_setopt($c3_curl, CURLOPT_USERAGENT, $h2[3]);
+curl_setopt($c3_curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3');
 $res3 = curl_exec($c3_curl);
 curl_close($c3_curl);
 
 $i6 = json_decode($res3, true);
 $d7 = $i6["js"]["cmd"];
 
-header("Location: " . $d7);
-die;
+header("Location: ".$d7);
+die; 
 
+?>
