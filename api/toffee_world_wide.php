5d0cc297c4fc370a6873ac186daced7400802b0f
BDIXTV <159545358+MohammadJoyChy@users.noreply.github.com>
2025-04-27 14:19:46 +0600

    Update toffee_world_wide.php

--- api/toffee_world_wide.php
+++ api/toffee_world_wide.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 // Initialize cURL session
 $ch = curl_init();
 
@@ -15,10 +15,6 @@ $response = curl_exec($ch);
 if (curl_errno($ch)) {
     echo "cURL Error: " . curl_error($ch);
 } else {
-    // Define the entry to add at the top
-    $new_entry = '#EXTINF:-1 tvg-logo="https://i.ibb.co.com/5gVjqSh0/Red-Abstract-Live-Stream-Free-Logo-20250309-192127-0002.png" group-title="𝗝𝗢𝗜𝗡 𝗢𝗨𝗥 𝗧𝗘𝗟𝗘𝗚𝗥𝗔𝗠", @bdixtv_official
-https://bdixtv.short.gy/bdixtv_official.m3u8' . PHP_EOL;
-
     // Append Referer to m3u8 URLs
     $modified_response = preg_replace_callback(
         '/https?:\/\/[^\s"\']+\.m3u8/',
@@ -28,15 +24,8 @@ https://bdixtv.short.gy/bdixtv_official.m3u8' . PHP_EOL;
         $response
     );
 
-    // Prepend the new entry to the modified response
-    $final_response = $new_entry . $modified_response;
-
-    // Set the correct headers for m3u playlist
-    header('Content-Type: audio/x-mpegurl');
-    header('Content-Disposition: inline; filename="playlist.m3u"');
-
-    // Output the response as a .m3u playlist
-    echo $final_response;
+    // Print the modified response
+    echo $modified_response;
 }
 
 curl_close($ch);
