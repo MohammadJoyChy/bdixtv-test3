282b768a889008e503a3d9d2d2bff08b1baa7e69
BDIXTV <159545358+MohammadJoyChy@users.noreply.github.com>
2025-04-27 15:11:03 +0600

    Update toffee_world_wide.php

--- api/toffee_world_wide.php
+++ api/toffee_world_wide.php
@@ -4,7 +4,7 @@
 
 // Main playlist URL
 
-$url = "https://raw.githubusercontent.com/byte-capsule/Toffee-Channels-Link-Headers/refs/heads/main/toffee_OTT_Navigator.m3u";
+$url = "https://host.mafiatv.live/toffee/kaya_app.php?route=getIPTVList";
 
 
 
