ceeef3bd7d6558beb6d7bbf2cee25b4f0fe92afc
BDIXTV <159545358+MohammadJoyChy@users.noreply.github.com>
2025-05-05 14:45:26 +0600

    Update toffee_world_wide.php

--- api/toffee_world_wide.php
+++ api/toffee_world_wide.php
@@ -4,7 +4,7 @@
 
 // Main playlist URL
 
-$url = "https://raw.githubusercontent.com/byte-capsule/Toffee-Channels-Link-Headers/refs/heads/main/toffee_OTT_Navigator.m3u";
+$url = "https://tv.bdixtv24.com/toffee/rriptv_app.php?route=getIPTVList";
 
 
 
